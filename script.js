cd765cb959f2347cdfcdb96ec3d9884e7facea94
Glitch (hello-webpage) <none>
2020-04-03 15:18:45 +0000

    🚃🕌 Checkpoint ./script.js:491517/1630 ./index.html:491517/4954 ./style.css:491517/8438 ./.glitch-assets:491517/1116

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+var main_field = document.querySelector(".field");
+var banner_container = document.querySelector(".banner-container");
+var banner1 = document.querySelector(".banner1");
+var player = document.querySelector(".player");
+var field_transform = 65;
+var incrementFieldIsTrue = false;
+
+
+function isIncrementFieldTrue() {
+    window.addEventListener("mousedown", function(){
+        incrementFieldIsTrue = true;
+        player.style.animation = "animate-char6 steps(4) reverse infinite 1s";
+
+    })
+    
+    window.addEventListener("mouseup", function(){
+        incrementFieldIsTrue = false;
+        player.style.animation = "";
+    })
+
+    window.addEventListener("mousescroll", function(e){
+        e.preventDefault();
+        incrementFieldIsTrue = true;
+    })
+}
+
+function incrementFieldTransform(){
+    if (incrementFieldIsTrue && field_transform >= -9610){
+        field_transform -= 5;
+        // player.innerHTML = field_transform;
+    }
+
+    if (field_transform <= -435 && field_transform >= -1435) {
+        banner1.style.transform = "translateY(460px)";
+    } else {
+        banner1.style.transform = "translateY(0px)";
+    }
+
+    main_field.style.transform = "translateX(" + field_transform + "px)";
+    banner_container.style.transform = "translateX(" + field_transform + "px)";
+}
+
+function play(){
+    incrementFieldTransform();
+}
+
+window.addEventListener("load", function(){
+    isIncrementFieldTrue();
+    setInterval(function(){
+        play();
+    }, 15);
+});
